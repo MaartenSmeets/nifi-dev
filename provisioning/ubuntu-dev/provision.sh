0f832453c59f8e13401de11841f6cf4cec3f7950
maartensmeets <wisp1980@gmail.com>
2021-12-24 13:13:40 +0100

    Graphics controller and font issue

--- provisioning/ubuntu-dev/provision.sh
+++ provisioning/ubuntu-dev/provision.sh
@@ -10,7 +10,7 @@ apt-get install --no-install-recommends ubuntu-desktop
 apt-get -y install aptitude
 aptitude -y install --without-recommends ubuntu-desktop 
 apt-get -y install gnome-session gnome-terminal tasksel terminator firefox jq apt-transport-https ca-certificates gnupg2 curl software-properties-common docker-ce docker-compose libxss1 libgconf-2-4 evince socat maven openjdk-11-jdk xfonts-terminus fonts-terminus console-terminus mlocate
-#tasksel install ubuntu-desktop-minimal
+tasksel install ubuntu-desktop-minimal
 
 #Fix root not allowed to start X-window
 xhost local:root
